c7b32a5ddebbc0b4269a445736c6af3383e0fd16
heshananupama <heshananupa@gmail.com>
2016-07-05 00:21:18 +0530

    07-05

--- AutoHub/Scripts/app.js
+++ AutoHub/Scripts/app.js
@@ -20,3 +20,5 @@
 });
 
  
+   $("#carousel").carousel();
+ 
--- AutoHub/Scripts/scripts.js
+++ AutoHub/Scripts/scripts.js
@@ -1,12 +1,16 @@
 
-jQuery(document).ready(function() {
+
+/*
+        Welcome screen Script 
+ */
+jQuery(document).ready(function () {
 	
     /*
         Fullscreen background
     */
     $.backstretch([
-                    "assets/img/backgrounds/1.jpg"
-	              , "assets/img/backgrounds/2.jpg"
+                    "assets/img/backgrounds/2.jpg"
+	              , "assets/img/backgrounds/1.jpg"
 	              , "assets/img/backgrounds/3.jpg"
 	             ], {duration: 3000, fade: 750});
     
@@ -16,20 +20,10 @@ jQuery(document).ready(function() {
     $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
     });
-    
-    $('.login-form').on('submit', function(e) {
-    	
-    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
-    		}
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-    	
-    });
+   
     
     
 });
+/*
+       End of  Welcome screen Script 
+ */
