405ca394cfb3b744a92113c810d7315db983b179
heshananupama <heshananupama@gmail.com>
2016-10-01 23:21:03 +0530

    10-01-2016

---
+++ AutoHubServices/Models/UsersRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoHubServices.Models
+{
+    public class UsersRepository
+    {
+        private static AutohubEntities dataContext = new AutohubEntities();
+        public static List<User> GetAllUsers()
+        {
+            var query = from User in dataContext.Users
+                        select User;
+            return query.ToList();
+        }
+    }
+} 
